9b2dd659b28aeaaf6dc9f9a4906652b0ec69da41
Amit Deshpande <amit.deshpande.tech@gmail.com>
2025-04-13 16:15:12 +0530

    Initial setup

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`your server is listening at port ${PORT}`);
+});
